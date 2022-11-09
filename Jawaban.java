26282f0e24400e746211ed9ec089092f30813027
rakhaputra2910 <115854656+rakhaputra2910@users.noreply.github.com>
2022-11-09 18:36:43 +0700

    Create Jawaban.java

---
+++ Jawaban.java
@@ -0,0 +1,31 @@
+public class Jawaban {
+public static void main (String [] args){
+byte value_byte = 100;
+short value_short = 30000;
+int value_int = 1000000;
+long value_long = 3000000000L;
+System.out.println("Nilai tipe data byte = " + value_byte);
+System.out.println("Nilai tipe data short = " + value_short);
+System.out.println("Nilai tipe data int = " + value_int);
+System.out.println("Nilai tipe data long = " + value_long);
+  
+float short_pi = 3.14f;
+double long_pi = 3.141592653589793d;
+System.out.println("Nilai short pi = " + short_pi);
+System.out.println("Nilai tipe long pi = " + long_pi);
+  
+char huruf_A = 'A', huruf_A_juga = 65;
+char huruf_B = 'B', huruf_B_juga = 66;
+char persen = '%', persen_juga = 37;
+char tandakutip = '\'';
+System.out.println(huruf_A+" dan "+huruf_A_juga);
+System.out.println(huruf_B+" dan "+huruf_B_juga);
+System.out.println(persen+" dan "+persen_juga);
+System.out.println(tandakutip);
+  
+boolean ApakahDosenAlproKeren = true;
+boolean ApakahDosenAlproJelek = false;
+System.out.println(ApakahDosenAlproKeren);
+System.out.println(ApakahDosenAlproJelek);
+}
+}
